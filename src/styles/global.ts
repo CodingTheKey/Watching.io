1560b5f5ddbd78664f7e5aedda18dee26af9ef4e
Nikolas-as <nikkolas21082003@gmail.com>
2022-01-29 21:07:38 -0300

    first commit :tada:

---
+++ src/styles/global.ts
@@ -0,0 +1,53 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+  }
+
+  html {
+    scroll-behavior: smooth;
+  }
+
+  body { 
+    background-color: ${({ theme }) => theme.colors.background};
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  /* ::-webkit-scrollbar {
+    width: 5px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background-color: ${({ theme }) => theme.colors.background};
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme.colors.blue};
+    border-radius: 5px;
+  } */
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%; //15px
+    }
+    
+    @media (max-width: 720px) {
+      font-size: 87.5%; //14px
+    }
+  }
+`;
---
+++ src/styles/theme.ts
@@ -0,0 +1,31 @@
+export default {
+  colors: {
+    background: "#09090B",
+    backgroundLight: "#0E0E11",
+
+    descriptionText: "#E1E3EA",
+    footerText: "#A8A8B3",
+    white: "#FFFFFF",
+    subtitleText: "#B4B4B4",
+    subjectText: "#D2D5DF",
+    titleCard: "#F9F9FB",
+    borderColor: "#D2D5DF",
+    buttonColor: "#272327",
+    footerFooter: "#A7A7A7",
+    inputColor: "#262627",
+
+    blue: "#2393FE",
+    darkenBlue: "#113254",
+
+    red: "#FF5757",
+    darkenRed: "#532022",
+
+    orange: "#FF8A00",
+    darkenOrange: "#533009",
+
+    green: "#55FF7A",
+    darkenGreen: "#23532C",
+
+    yellow: "#FFAC24",
+  },
+};
