0767ff2c61eb05afdd0890a5bb8155cada085e03
José Corte <josecarlos-corte@hotmail.com>
2022-02-08 14:58:18 -0300

    :sparkles: adding progress bar (nprogress)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,35 @@ import theme from '../styles/theme';
 import '../styles/global.ts'
 import { Header } from '../components/Header';
 
+import { useEffect } from 'react'
+
+import { useRouter } from 'next/router'
+import NProgress from 'nprogress'
+import '../styles/nprogress/nprogress.css'
+
 function MyApp({ Component, pageProps }: AppProps) {
+
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleStart = (url) => {
+      console.log(`Loading: ${url}`)
+      NProgress.start()
+    }
+    const handleStop = () => {
+      NProgress.done()
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleStop)
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleStop)
+      router.events.off('routeChangeError', handleStop)
+    }
+  }, [router])
   return (
     <ThemeProvider theme={theme}>
       <Header />
